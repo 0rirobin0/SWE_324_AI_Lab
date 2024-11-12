00aa4bc72680abce0678c0e60ec8739a6f5b617d
robin <rirobink@gmail.com>
2024-11-12 22:27:38 +0600

    first commit

---
+++ bfs.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = 1e5 + 2;
+bool Vis[N];
+vector<int> adj[N];
+int main()
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        Vis[i] = 0;
+    }
+
+//    Taking input of graph in adjacency list
+ 
+    int n,m;
+    cin>>n>>m;
+    for (int i = 0; i < m; i++)
+    {
+      int x,y;
+      cin>>x>>y;
+      adj[x].push_back(y);
+      adj[y].push_back(x);
+    }
+
+    // traversing the adj list  with BFS
+    
+    queue<int>q;
+    q.push(1);
+    Vis[1] = true;
+
+
+
+ while (!q.empty())
+ {
+    int current_node = q.front();
+    q.pop();
+    cout<<current_node<<endl;
+
+    vector<int> :: iterator it;
+    for (it= adj[current_node].begin(); it <adj[current_node].end() ; it++)
+    {
+       if(!Vis[*it])
+       {
+        q.push(*it);
+        Vis[*it]= true;
+       }
+    }
+    
+
+ }
+ 
+    
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ dfs.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+
+const int N = 1e5 + 2;
+bool Vis[N];
+vector<int> adj[N];
+
+
+
+void dfs(int node)
+{
+    // pre-order 
+    Vis[node]= true;
+     cout<< node <<" ";
+
+    // inorder
+    vector<int> :: iterator it;
+    for ( it = adj[node].begin(); it!=adj[node].end(); it++)
+    {
+        if(!Vis[*it])
+        {
+            dfs(*it);
+        }
+    }
+
+// post order
+    // cout<< node <<" ";
+    
+}
+
+
+
+
+
+
+int main()
+{
+
+     for (int i = 0; i < N; i++)
+    {
+        Vis[i] = 0;
+    }
+
+//    Taking input of graph in adjacency list
+ 
+    int n,m;
+    cin>>n>>m;
+    for (int i = 0; i < m; i++)
+    {
+      int x,y;
+      cin>>x>>y;
+      adj[x].push_back(y);
+      adj[y].push_back(x);
+    }
+
+    dfs(1);
+
+
+ return 0;
+}
